1e59f434fd78f1fc13cfa78a613641fc78d9d55f
mane <mane.gabrielian@gmail.com>
2016-01-11 16:10:57 +0300

    add

---
+++ Demo/Demo/Songs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Songs
+    {
+        private int _id; 
+
+        public int Id 
+        { 
+        get { return _id; } 
+        set { _id = value; } 
+        } 
+
+        private string _name; 
+
+        public string Name 
+        { 
+        get { return _name; } 
+        set { _name = value; } 
+        } 
+
+        private Singer _singer; 
+
+        public Singer Singer 
+        { 
+        get { return _singer; } 
+        set { _singer = value; } 
+        } 
+    }
+}
