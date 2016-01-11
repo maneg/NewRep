91a7ad3dc7b20f809fc71787baec58ba094c38ec
ValeriaArkhipova <makhinatorbreetnos@gmail.com>
2016-01-11 16:19:37 +0300

    Added context

--- Demo/Demo/Context.cs
+++ Demo/Demo/Context.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Demo
 {
-    class Context
+    class Context : DbContext 
     {
+        public DbSet <Songs> Songs { get; set; }
+        public DbSet <Singer> Singers { get; set; }
     }
 }
